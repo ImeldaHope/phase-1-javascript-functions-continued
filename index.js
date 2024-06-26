b72ba81f033fcd4c2d5da3fd81e5354c18690d4c
Imelda Hope <imeldalarusso@gmail.com>
2024-06-26 14:17:09 +0300

    defines function leve scope and iife

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // code your solution here
+
+function saturdayFun(fun = "roller-skate"){
+    return `This Saturday, I want to ${fun}!`;
+}
+
+saturdayFun("bathe my dog");
+
+const mondayWork = function(doList = "go to the office"){
+    return `This Monday, I will ${doList}.`;
+}
+
+mondayWork("work from home");
+
+function wrapAdjective(flair = "*") {
+    return (function(myParam = "special"){
+        return `You are ${flair}${myParam}${flair}!`;
+    })
+}
+
+wrapAdjective()();
